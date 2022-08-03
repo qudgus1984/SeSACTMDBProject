b39cc1e3ea31acb7c95d03405485a65e6660e473
ByeongHyeon <qudgus1984@naver.com>
2022-08-03 09:28:16 +0900

    Initial Commit

---
+++ SeSACTMDBProject/SearchViewController.swift
@@ -0,0 +1,10 @@
+//
+//  SearchViewController.swift
+//  SeSACTMDBProject
+//
+//  Created by 이병현 on 2022/08/03.
+//
+
+import Foundation
+
+var testFile = "filefile"
---
+++ SeSACTMDBProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACTMDBProject
+//
+//  Created by 이병현 on 2022/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

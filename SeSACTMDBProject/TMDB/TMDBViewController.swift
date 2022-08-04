e55f3c394aaff8258c997005335b0dc76ac0abd2
ByeongHyeon <qudgus1984@naver.com>
2022-08-05 01:29:56 +0900

    데이터 받아오기 수정 필요

--- SeSACTMDBProject/TMDB/TMDBViewController.swift
+++ SeSACTMDBProject/TMDB/TMDBViewController.swift
@@ -25,7 +25,23 @@ class TMDBViewController: UIViewController {
         TMDBCollectionView.delegate = self
         TMDBCollectionView.dataSource = self
 
+        CellLayout()
+        
         fetchTMDB()
+    }
+    
+    func CellLayout() {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 8
+        let width = UIScreen.main.bounds.width - (spacing * 2)
+        
+        layout.itemSize = CGSize(width: width / 1.1, height: (width / 1.1) * 1.2 )
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        
+        TMDBCollectionView.collectionViewLayout = layout
     }
     
     func fetchTMDB() {
